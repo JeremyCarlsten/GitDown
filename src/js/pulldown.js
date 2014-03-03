a4887990d1945e061ae780ae6149de79a76b45a3
JeremyCarlsten <jcc.993@gmail.com>
2014-03-03 08:31:05 -0600

    Added basic call to github api, with support for max results and username

--- src/js/pulldown.js
+++ src/js/pulldown.js
@@ -1,3 +1,14 @@
-/**
- * Created by pair11 on 3/3/14.
- */
+$.fn.pullDownGithubEvents = function (username, maxNumber) {
+    var element = $(this);
+    $.ajax("https://api.github.com/users/" + username + "/events", {dataType: 'json'})
+        .done(function (data) {
+            $(data).each(function (i, item) {
+                if (i <= maxNumber) {
+                    $(element).append('<p>' + item.type + '</p>');
+                }
+            });
+        })
+        .fail(function (jqxhr, textStatus, error) {
+            console.log("you've messed it all up." + error)
+        });
+};
